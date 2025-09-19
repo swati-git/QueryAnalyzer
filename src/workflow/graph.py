32016f88b40d93ace97e42617ab1136b5f60d11f
space <swati.phartiyal@gmail.com>
2025-09-19 16:29:20 +0530

    Use the set_conditional_entry_point API from langgraph (just because its an available feature), Replace  the START node and  query_analyzer_node with the set_conditional_entry_point API

--- src/workflow/graph.py
+++ src/workflow/graph.py
@@ -1,15 +1,23 @@
 from langgraph.graph import StateGraph
-from langgraph.constants import START, END
-from src.graph_nodes.query_analyzer_node import query_analyzer_node
+from langgraph.constants import  END
 from src.graph_state.state import GraphState
+from src.chains.query_router import query_router, QueryRouter
+
+def route_question(state: GraphState) -> str:
+    print("---ROUTE QUESTION---")
+    source: QueryRouter = query_router.invoke({"question": state["query"]})
+    return "WEBSEARCH" if source.datasource == "WEBSEARCH" else "RETRIEVE"
 
 
 workflow = StateGraph(GraphState)
 
-workflow.add_node("QueryAnalyzer", query_analyzer_node)
-
-workflow.add_edge(START, "QueryAnalyzer")
-workflow.add_edge("QueryAnalyzer", END)
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        "WEBSEARCH": "WEBSEARCH",
+        "RETRIEVE": "RETRIEVE",
+    },
+)
 
 application = workflow.compile()
 
