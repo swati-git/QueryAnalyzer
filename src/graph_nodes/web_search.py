ba257f9eedfc2720397c4617532c45d37a7243c5
space <swati.phartiyal@gmail.com>
2025-09-20 09:12:39 +0530

    feat: Integrate with Tavily Search in order to perform all web search operations

--- src/graph_nodes/web_search.py
+++ src/graph_nodes/web_search.py
@@ -1,7 +1,18 @@
 from src.graph_state.state import GraphState
 from typing import Any, Dict
+from langchain_tavily import TavilySearch
+from langchain.schema import Document
+from dotenv import load_dotenv
 
-def web_search(GraphState) -> Dict[str, Any]:
+web_search_tool = TavilySearch(max_results=3)
+
+load_dotenv()
+
+def web_search(graph_state: GraphState) -> Dict[str, Any]:
     print("---WEB SEARCH---")
-    # Simulate a web search result
+   
+    tavily_results = web_search_tool.invoke({"query" : graph_state["query"]})["results"]
+    complete_result = "\n".join([tavily_result["content"] for tavily_result in tavily_results])
+    web_results = Document(page_content=complete_result)
+    return {"response": web_results, "query": graph_state["query"]}
     return {"response": "This is a simulated web search result."}
